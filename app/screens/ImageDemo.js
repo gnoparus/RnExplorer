6dd4a36633198cb57cbe015046f6ee63a689f315
keng <surapong@gmail.com>
2017-05-23 18:52:29 +0700

    Change minimum zoom scale to 0.9

--- app/screens/ImageDemo.js
+++ app/screens/ImageDemo.js
@@ -3,7 +3,7 @@
  */
 
 import React, { Component } from "react";
-import { Text, Image, ScrollView, View, Dimensions } from "react-native";
+import { Text, Image, ScrollView, View, Dimensions, StyleSheet } from "react-native";
 
 import PhotoView from "react-native-photo-view";
 
@@ -12,16 +12,25 @@ const ImageDemo = () => (
 
     <PhotoView
       source={require("../../images/lao01.jpg")}
-      minimumZoomScale={0.5}
+      minimumZoomScale={0.9}
       maximumZoomScale={3}
-      rotate={true}
-      style={{
-        width: Dimensions.get("window").width,
-        height: Dimensions.get("window").height
-      }}
+    
+      style={styles.photo}
     />
 
   </View>
 );
+
+const styles = StyleSheet.create({
+  photo: {
+     width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height
+  }
+});
+
+
+
+
+
 
 export default ImageDemo;
