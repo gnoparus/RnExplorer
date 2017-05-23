0dd20def9386299cb8b33b89cf9a66d27771a266
keng <surapong@gmail.com>
2017-05-23 18:27:10 +0700

    Fix PhotoView dynamic width and height

--- app/screens/ImageDemo.js
+++ app/screens/ImageDemo.js
@@ -3,21 +3,25 @@
  */
 
 import React, { Component } from "react";
-import { Text, Image, ScrollView, View } from "react-native";
+import { Text, Image, ScrollView, View, Dimensions } from "react-native";
 
 import PhotoView from "react-native-photo-view";
 
 const ImageDemo = () => (
-  <ScrollView>
+  <View>
 
     <PhotoView
       source={require("../../images/lao01.jpg")}
       minimumZoomScale={0.5}
       maximumZoomScale={3}
-      style={{width: 300, height: 300}}
+      rotate={true}
+      style={{
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height
+      }}
     />
 
-  </ScrollView>
+  </View>
 );
 
 export default ImageDemo;
