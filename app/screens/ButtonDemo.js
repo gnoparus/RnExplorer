7b1243bb3d48b5a232c1d7a0f1065a567678bbf8
keng <surapong@gmail.com>
2017-05-23 17:11:06 +0700

    try different button styles and layout

--- app/screens/ButtonDemo.js
+++ app/screens/ButtonDemo.js
@@ -7,51 +7,53 @@ import { ScrollView, View, StyleSheet, Platform } from "react-native";
 import { Text, Button } from "react-native-elements";
 
 const ButtonDemo = () => (
-
   <ScrollView>
+    <View>
 
-    <Button title="BUTTON Normal" buttonStyle={styles.button} />
+      <Button title="BUTTON Normal" buttonStyle={styles.button} />
 
-    <Button raised icon={{ name: "cached" }} title="BUTTON WITH ICON"  buttonStyle={styles.button} />
+      <Button
+        raised
+        icon={{ name: "cached" }}
+        title="COLOR BUTTON WITH ICON"
+        color="yellow"
+        backgroundColor="red"
+        buttonStyle={styles.button}
+      />
 
-    <Button
-      large
-      iconRight
-      icon={{ name: "code" }}
-      title="LARGE WITH RIGHT ICON"
-       buttonStyle={styles.button}
-    />
+      <Button
+        large
+        iconRight
+        icon={{ name: "code" }}
+        title="LARGE WITH RIGHT ICON"
+        buttonStyle={styles.button}
+      />
 
-    <Button
-      large
-      icon={{ name: "envira", type: "font-awesome" }}
-      title="LARGE WITH RIGHT ICON"
-       buttonStyle={styles.button}
-    />
+      <Button
+        large
+        icon={{ name: "envira", type: "font-awesome" }}
+        title="LARGE WITH LEFT ICON"
+        buttonStyle={styles.button}
+      />
 
-    <Button
-      large
-      icon={{
-        name: "squirrel",
-        type: "octicon",
-        buttonStyle: { backgroundColor: "red", borderRadius: 10 }
-      }}
-      title="OCTICON"
-       buttonStyle={styles.button}
-    />
+      <Button
+        large
+        icon={{
+          name: "squirrel",
+          type: "octicon",
+        }}
+        title="OCTICON" 
+        buttonStyle={ {marginTop: 15, backgroundColor: "green", borderRadius: 10}}
+      />
+    </View>
 
   </ScrollView>
-  
 );
 
 const styles = StyleSheet.create({
-  
   button: {
-    marginTop: 15,
-    
-
-  },
- 
+    marginTop: 15
+  }
 });
 
 export default ButtonDemo;
